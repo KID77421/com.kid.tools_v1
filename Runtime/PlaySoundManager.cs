991cabe28d757734b923be08d0feee6084ea206a
KID <57336657+KID77421@users.noreply.github.com>
2024-11-20 19:41:51 +0800

    Initial commit

---
+++ Runtime/PlaySoundManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace KID.Tools
+{
+    /// <summary>
+    /// 播放音效管理器
+    /// </summary>
+    public class PlaySoundManager : MonoBehaviour
+    {
+        /// <summary>
+        /// 播放音效
+        /// </summary>
+        /// <param name="clip">音效</param>
+        private void PlaySound(AudioClip clip)
+        {
+            SoundManager.instance.PlaySound(clip);
+        }
+    }
+}
